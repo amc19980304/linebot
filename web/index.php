7d944edeb7e6a711f73141a4313eb4742e47929a
amc19980304 <40534419+amc19980304@users.noreply.github.com>
2018-06-24 15:13:00 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -30,7 +30,7 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                	$m_message = 'fuck you mother';
+                	$m_message = $message['text'];
                 	if($m_message!="")
                 	{
                 		$client->replyMessage(array(

ca131be06f6401db6b45c88a14f29b50e0b091ac
amc19980304 <40534419+amc19980304@users.noreply.github.com>
2018-06-27 13:22:54 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,28 +1,7 @@
 <?php
-
-/**
- * Copyright 2016 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
-
 require_once('./LINEBotTiny.php');
-
-
-
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
@@ -33,15 +12,20 @@ foreach ($client->parseEvents() as $event) {
                 	$m_message = $message['text'];
                 	if($m_message!="")
                 	{
-                		$client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $m_message
-                            )
-                        )
-                    	));
+                            $client->replyMessage(array(
+                                'replyToken' => $event['replyToken'],
+                                'messages' => array(
+                                    array(
+                                        'type' => 'text',
+                                        'text' => $m_message
+                                    ),
+                                    array(
+                                        'type' => 'image',
+                                        'originalContentUrl' => 'https://github.com/amc19980304/linebot/blob/master/menu.jpg：',
+                                        'previewImageUrl' => 'https://github.com/amc19980304/linebot/blob/master/menu.jpg'
+                                    )
+                                )
+                            ));
                 	}
                     break; 
                 

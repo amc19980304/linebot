a0422f35d26afbb226f58d490aaf965c91600c5d
amc19980304 <40534419+amc19980304@users.noreply.github.com>
2018-06-28 14:50:07 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -151,21 +151,6 @@ foreach ($client->parseEvents() as $event) {
                                                         'type' => 'postback', // 類型 (連結)
                                                         'label' => '姓名', // 標籤 3
                                                         'data' => 'type=name' // 連結網址
-                                                    ),
-                                                    array(
-                                                        'type' => 'postback', // 類型 (連結)
-                                                        'label' => '電話', // 標籤 3
-                                                        'data' => 'type=phone' // 連結網址
-                                                    ),
-                                                    array(
-                                                        'type' => 'postback', // 類型 (連結)
-                                                        'label' => '地址', // 標籤 3
-                                                        'data' => 'type=address' // 連結網址
-                                                    ),
-                                                    array(
-                                                        'type' => 'postback', // 類型 (連結)
-                                                        'label' => '訂餐內容', // 標籤 3
-                                                        'data' => 'type=content' // 連結網址
                                                     )
                                                 )
                                             ),
